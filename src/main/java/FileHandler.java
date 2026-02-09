d396a45a792b3e67029142bd6049fe8d743b2d34
KemalGC <kemalgcortuk@gmail.com>
2026-02-09 15:23:34 -0500

    Fixed FileHandler to only return .txt files

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -3,8 +3,12 @@ import java.io.File;
 public class FileHandler {
 
     public File[] getAvailableFiles(){
-        File[] folder = new File("data").listFiles();
-        return folder;
+        File folder = new File("data");
+
+        //use .getName() when iterating for names
+        File[] textFiles = folder.listFiles((dir,name)->name.endsWith("txt"));
+
+        return textFiles;
     }
 
     public String getFileContents(int fileID){return "";}
