cf7d9ca01a0d131a9909bf382844f404ffb694e0
KemalGC <kemalgcortuk@gmail.com>
2026-02-10 18:42:49 -0500

    getAvailableFilesReturnsTextFiles

--- src/test/java/FileHandlerTest.java
+++ src/test/java/FileHandlerTest.java
@@ -1,13 +1,23 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FileHandlerTest {
 
 
     @Test
-    public void testAdd() {
-        int result = Math.abs(4-1);
-        assertEquals(3, result);
+    public void getAvailableFilesReturnsTextFiles() {
+        FileHandler handler = new FileHandler();
+        File[] files = handler.getAvailableFiles();
+
+        ArrayList<String> fileNames = new ArrayList<>(Arrays.stream(files).map(File::getName).toList());
+
+        assertEquals(2, fileNames.size());
+        assertTrue(fileNames.contains("carnivore.txt"));
+        assertTrue(fileNames.contains("cointelpro.txt"));
     }
 }
