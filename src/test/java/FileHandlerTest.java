5d42916152e9f210f45a627780a3b7a26575582d
KemalGC <kemalgcortuk@gmail.com>
2026-02-10 13:19:32 -0500

    Fixing Testing Issue

---
+++ src/test/java/FileHandlerTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileHandlerTest {
+
+
+    @Test
+    public void testAdd() {
+        int result = Math.abs(4-1);
+        assertEquals(3, result);
+    }
+}
